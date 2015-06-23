1f75da14d12d1e9eb4e81368b732781427ca5ed7
Tristone <tristone.hua@autodesk.com>
2015-06-23 18:12:22 +0800

    Add create new Menu

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, Base, MenuItem
@@ -20,8 +20,17 @@ def showRestaurant(restaurant_id):
 # Task 1: Create route for newMenuItem function here
 
 
-@app.route('/restaurant/<int:restaurant_id>/new/')
+@app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
+    if request.method == 'POST':
+        newItem = MenuItem(
+            name=request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('showRestaurant', restaurant_id=restaurant_id))
+    else:
+        return render_template("newmenuitem.html", restaurant_id=restaurant_id)
+
     return "page to create a new menu item. Task 1 complete!"
 
 # Task 2: Create route for editMenuItem function here
